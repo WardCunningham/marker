534e053ce0575d8490995648cdfb45b5fea36307
WardCunningham <ward@AboutUs.org>
2010-07-29 20:53:44 -0700

    recognize mac newlines in verbatim text

--- test/verbatim_test.rb
+++ test/verbatim_test.rb
@@ -6,14 +6,21 @@ class VerbatimTest < Test::Unit::TestCase
   def test_preformatted_text
     text = " some source code"
     markup = Marker.parse text
-    
+
     assert_match("<pre>\nsome source code\n</pre>", markup.to_html)
   end
 
   def test_multiline_preformatted_text
     text = " some source code\n some more source code"
     markup = Marker.parse text
-    
+
+    assert_match("<pre>\nsome source code\nsome more source code\n</pre>", markup.to_html)
+  end
+
+  def test_multiline_preformatted_text_from_mac
+    text = " some source code\r some more source code"
+    markup = Marker.parse text
+
     assert_match("<pre>\nsome source code\nsome more source code\n</pre>", markup.to_html)
   end
 
